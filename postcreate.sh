deb2105f2cdaed608eed9767a87b475e89f64858
Arul Adaikalam <arul.adaikalam@navitas.com>
2024-06-20 08:24:28 +0800

    explicit specification of home dir

--- postcreate.sh
+++ postcreate.sh
@@ -1,7 +1,8 @@
 CURRENT_VERSION=2.36.7
 wget https://github.com/Versent/saml2aws/releases/download/v${CURRENT_VERSION}/saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz
-tar -xzvf saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz -C ~/
+tar -xzvf saml2aws_${CURRENT_VERSION}_linux_amd64.tar.gz -C /home/node
+cd /home/node
 chmod u+x ~/saml2aws
 cd /usr/local/bin
-sudo ln -s ~/saml2aws
+sudo ln -s /home/node/saml2aws
 sudo chmod +x node *
